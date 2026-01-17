28d4bab6ab9a5a6722effc59bf74d4d7fdc786a7
PulockKundu <pulock7643@gmail.com>
2026-01-18 01:45:55 +0600

    Sky Background Added

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -1,43 +1,37 @@
 #include <GL/glut.h>
-
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
+#include <math.h>
 
 
-     glBegin(GL_TRIANGLES);
-     glColor3f(1.0f, 0.0f, 0.0f);
-glVertex2f(-0.3f, 0.0f);
-glVertex2f(0.3f, -0.0f);
-glVertex2f(0.0f, 0.5f);
 
-glColor3f(0.0f, 1.0f, 0.0f);
-glColor3f(0.0f, 0.0f, 1.0f);
-glEnd();
-  glBegin(GL_POLYGON);
-        glColor3f(1, 0, 0);
-        glVertex2f(0.3, 0.0);
-
-        glColor3f(0, 1, 0);
-        glVertex2f(-0.3, 0.0);
-
-        glColor3f(0, 0, 1);
-        glVertex2f(-0.3, -0.5);
-
-        glColor3f(1, 0, 0);
-        glVertex2f(0.3, -0.5);
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);  
+     glColor3f(0.5, 0.8, 0.9); 
+    glBegin(GL_QUADS);
+        glVertex2f(4.0, -4.0);
+        glVertex2f(4.0, 7.0);
+        glVertex2f(-4.0, 7.0);
+        glVertex2f(-4.0, -4.0);
     glEnd();
 
-    glFlush();
+
+ glutSwapBuffers();
 }
 
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640, 500);
-    glutCreateWindow("OpenGL 64bit");
-    glutDisplayFunc(display);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glutMainLoop();
+void myInit() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity(); 
+    gluOrtho2D(-4.0, 4.0, -4.0, 7.0);  
+    glMatrixMode(GL_MODELVIEW);  
+}
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);  
+    glutInitWindowSize(1400, 750); 
+    glutCreateWindow("Graphics Project");  
+    myInit();  
+    glutDisplayFunc(display);  
+    //glutTimerFunc(30, updatePositionCloud, 0);  
+    // glutTimerFunc(30, updatePositionRocket, 0);  
+    glutMainLoop();  
     return 0;
 }
